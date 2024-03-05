25607a0eecd901c39130df5730fe82b647896a0d
yanuvelir <yanuvelir@gmail.com>
2024-03-05 20:20:16 +0200

    Edited Box class, MouseEvent + AI

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -13,16 +13,17 @@ public class Box extends JPanel {
         cell = new Cell();
         setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);
         setBackground(Config.getColor(Status.NONE));
-    }
-
-    public void setColor() {
-        setBackground(Config.getColor(cell.status));
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 cell.turn();
+                setColor();
             }
         });
+    }
+
+    public void setColor() {
+        setBackground(Config.getColor(cell.status));
     }
 
     void step1() {
@@ -35,3 +36,4 @@ public class Box extends JPanel {
         setColor();
     }
 }
+
